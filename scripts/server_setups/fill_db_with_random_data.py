32e18e1911359062cba20367cb66238241fc9fdb
marcoprado17 <marco.pdsv@gmail.com>
2017-01-25 22:28:36 -0200

    ...

--- scripts/server_setups/fill_db_with_random_data.py
+++ scripts/server_setups/fill_db_with_random_data.py
@@ -8,10 +8,10 @@ import sys
 sys.path.append("/vagrant")
 sys.path.append("/vagrant/build")
 
+from app_contexts.app import app
+
 from models.utils import create_random_product_categories, create_random_products, create_specif_cities, create_random_clients, create_random_orders, \
     create_random_product_subcategories, create_random_blog_posts
-
-from app_contexts.app import app
 
 
 def fill_db_with_random_data():
