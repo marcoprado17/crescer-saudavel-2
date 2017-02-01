56900d6fe7569e3377cf476432fa70fecd675370
marcoprado17 <marco.pdsv@gmail.com>
2017-02-01 20:18:00 -0200

    Adding ideal size auxiliar text in admin add image page

--- src/r.py
+++ src/r.py
@@ -11,6 +11,9 @@ from flask_bombril.utils import stringfy_list
 class Resources(object):
     # noinspection PyPep8Naming
     class string(object):
+        ideal_product_image_size_auxiliar_text = "Tamanho ideal das imagens de produto: 600 x 600."
+        ideal_blog_thumbnail_size_auxiliar_text = "Tamanho ideal das thumbnails do blog: 900 x 500."
+        ideal_carousel_images_size_auxiliar_text = "Tamanho ideal das imagens do carousel: 2560 x 500."
         post_thumbnail = "Thumbnail do post"
         order_by = "Ordenar por: "
         inner_link_example = "Ex.: /produtos/?category_id=1"
