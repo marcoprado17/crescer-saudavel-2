baec4471850b8d72aa60ea51936f8652ffac7138
marcoprado17 <marco.pdsv@gmail.com>
2017-05-27 13:02:23 -0300

    Refactoring exchanges_and_returns model and his view

--- src/models_view/content/exchanges_and_returns_view.py
+++ src/models_view/content/exchanges_and_returns_view.py
@@ -9,12 +9,11 @@ class ExchangesAndReturnsView(BaseContentView):
     name = R.string.exchanges_and_returns
     endpoint = R.string.exchanges_and_returns_endpoint
 
-    column_list = ['content_html']
     column_formatters = dict(
         content_html=ProjBaseView.html_formatter
     )
+    column_list = ['content_html']
 
-    form_excluded_columns = ["content_html"]
     form_args = dict(
         content_markdown=dict(
             render_kw=dict(
@@ -22,6 +21,7 @@ class ExchangesAndReturnsView(BaseContentView):
             )
         )
     )
+    form_excluded_columns = ["content_html"]
     form_rules = (
         rules.Field('content_markdown', render_field='markdown_text'),
     )
