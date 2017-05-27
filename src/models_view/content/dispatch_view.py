7d7630905e2df88911a387d6cc7d79292dfb3789
marcoprado17 <marco.pdsv@gmail.com>
2017-05-27 13:01:49 -0300

    Refactoring dispatch model and his view

--- src/models_view/content/dispatch_view.py
+++ src/models_view/content/dispatch_view.py
@@ -1,5 +1,4 @@
 from flask_admin.form import rules
-
 from models_view.content.base_content_view import BaseContentView
 from models_view.proj_base_view import ProjBaseView
 from r import R
@@ -9,12 +8,11 @@ class DispatchView(BaseContentView):
     name = R.string.dispatch
     endpoint = R.string.dispatch_endpoint
 
-    column_list = ['content_html']
     column_formatters = dict(
         content_html=ProjBaseView.html_formatter
     )
+    column_list = ['content_html']
 
-    form_excluded_columns = ["content_html"]
     form_args = dict(
         content_markdown=dict(
             render_kw=dict(
@@ -22,6 +20,7 @@ class DispatchView(BaseContentView):
             )
         )
     )
+    form_excluded_columns = ["content_html"]
     form_rules = (
         rules.Field('content_markdown', render_field='markdown_text'),
     )
